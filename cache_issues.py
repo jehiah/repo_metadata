d73dceb512ea31b0b4ad467c798f2eb8345c1e17
Jehiah Czebotar <jehiah@gmail.com>
2017-03-22 11:02:50 -0400

    issue stats by month

--- cache_issues.py
+++ cache_issues.py
@@ -25,19 +25,17 @@ def run():
     global endpoint
     token = tornado.options.options.access_token
     endpoint = endpoint % tornado.options.options.repo
-    url = endpoint + urllib.urlencode(dict(access_token=token, per_page=100, filter='all', state='closed'))
-    logging.info('fetching closed issues for %r', tornado.options.options.repo)
-    raw_issues = fetch_all(url, limit=tornado.options.options.limit, callback=cache_issues)
-    url = endpoint + urllib.urlencode(dict(access_token=token, per_page=100, filter='all', state='open'))
-    logging.info('fetching open issues for %r', tornado.options.options.repo)
-    raw_issues = fetch_all(url, limit=tornado.options.options.limit, callback=cache_issues)
-    logging.info("got %d", len(raw_issues))
-    
+    for state in tornado.options.options.state:
+        url = endpoint + urllib.urlencode(dict(access_token=token, per_page=100, filter='all', state=state))
+        logging.info('fetching %s issues for %r', state, tornado.options.options.repo)
+        raw_issues = fetch_all(url, limit=tornado.options.options.limit, callback=cache_issues)
+        logging.info("got %d %s issues", len(raw_issues), state)
 
 if __name__ == "__main__":
     tornado.options.define("repo", default=None, type=str, help="user/repo to query")
     tornado.options.define("access_token", type=str, default=None, help="github access_token")
     tornado.options.define("issue_cache_dir", type=str, default="../repo_cache/issue_cache", help="directory to cache issues")
+    tornado.options.define("state", type=str, default=["open", "closed"], multiple=True)
     tornado.options.define("limit", type=int, default=1000)
     tornado.options.parse_command_line()
     
---
+++ issue_stats.py
@@ -0,0 +1,77 @@
+import json
+import tornado.options
+import datetime
+import os
+import glob
+from collections import defaultdict
+from operator import itemgetter
+from formatters import _github_dt
+
+
+
+def load_data(min_dt):
+    for issue_file in glob.glob(os.path.join(tornado.options.options.issue_cache_dir, '*.json')):
+        with open(issue_file, 'r') as f:
+            issue = json.load(f)
+            dt = _github_dt(issue['created_at'])
+            if dt < min_dt:
+                continue
+            login = issue["user"]["login"]
+            for assignee in issue["assignees"]: # pick a random one?
+                login = assignee["login"]
+            yield dict(
+                issue_number=issue["number"],
+                login=login,
+                created_at=dt,
+                labels = map(itemgetter('name'), issue["labels"]),
+            )
+
+def build_table(records, f=None):
+    if callable(f):
+        records = filter(f, records)
+    def inner():
+        return defaultdict(int)
+    data = defaultdict(inner)
+    for row in records:
+        data[row['login']][row['created_at'].strftime("%Y/%m")] += 1
+    
+    columns = set()
+    for datasets in data.values():
+        columns |= set(datasets.keys())
+    
+    columns = sorted(columns)
+    rows = []
+    for login in sorted(data.keys()):
+        total = sum(data[login].values()) / len(columns)
+        rows.append(["%14s" % login] + map(lambda x: "%3d" % data[login][x], columns) + ["%3d" % total])
+    
+    return ["%14s " % "login"] + map(lambda x: x[2:], columns) + [" avg"], rows
+
+
+
+
+if __name__ == "__main__":
+    # tornado.options.define("comment_cache_dir", type=str, default="../repo_cache/comment_cache", help="directory to cache comments")
+    tornado.options.define("issue_cache_dir", type=str, default="../repo_cache/issue_cache", help="directory to cache issues")
+    tornado.options.define("min_dt", type=str, default=datetime.datetime(2016,1,1).strftime('%Y-%m-%d'), help="YYYY-mm-dd")
+    
+    tornado.options.parse_command_line()
+    
+    min_dt = datetime.datetime.strptime(tornado.options.options.min_dt, '%Y-%m-%d')
+    records = list(load_data(min_dt))
+    
+    print ""
+    print "PR's by assignee by month created"
+    columns, rows = build_table(records) 
+    print "|".join(columns)
+    for row in rows:
+        print " | ".join(row)
+
+    print ""
+    print "PR's with \"bug\" label by assignee by month created"
+    columns, rows = build_table(records, lambda x: 'bug' in x['labels'])
+    print "|".join(columns)
+    for row in rows:
+        print " | ".join(row)
+    
+    
